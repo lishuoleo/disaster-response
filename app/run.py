f9d92a353a9a89b7c508d46b570c880b4b027727
lishuoleo <lishuons13@gmail.com>
2020-02-17 22:23:36 +0800

    Deploy model version 1.

--- app/run.py
+++ app/run.py
@@ -3,7 +3,11 @@ import plotly
 import pandas as pd
 
 from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk import pos_tag, word_tokenize
+import nltk
+
+from sklearn.base import BaseEstimator, TransformerMixin
 
 from flask import Flask
 from flask import render_template, request, jsonify
@@ -13,6 +17,24 @@ from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
 
 def tokenize(text):
     tokens = word_tokenize(text)
@@ -26,26 +48,31 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('DisasterResponse', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
+
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+
+    category_names = df.iloc[:,4:].columns
+    category_boolean = (df.iloc[:,4:] != 0).sum().values
+
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
+            # GRAPH 1 - genre graph
         {
             'data': [
                 Bar(
@@ -63,13 +90,33 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+            # GRAPH 2 - category graph
+        {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_boolean
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category",
+                    'tickangle': 35
+                }
+            }
         }
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -78,13 +125,13 @@ def index():
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
         'go.html',
         query=query,
--- data/process_data.py
+++ data/process_data.py
@@ -1,37 +1,83 @@
+"""
+PREPROCESSING DATA
+Disaster Response Pipeline Project
+Udacity - Data Science Nanodegree
+
+Sample Script Execution:
+> python process_data.py disaster_messages.csv disaster_categories.csv DisasterResponse.db
+
+Arguments:
+    1) CSV file containing messages (disaster_messages.csv)
+    2) CSV file containing categories (disaster_categories.csv)
+    3) SQLite destination database (DisasterResponse.db)
+"""
+
 import sys
+import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
-
+ 
 def load_data(messages_filepath, categories_filepath):
-    messages = pd.read_csv('disaster_messages.csv')
-    categories = pd.read_csv('../data/disaster_categories.csv')
-    df = pd.merge(messages, categories, how = 'right', on = 'id')
-
-    return df
-
+    """
+    Load Data function
+    
+    Arguments:
+        messages_filepath -> path to messages csv file
+        categories_filepath -> path to categories csv file
+    Output:
+        df -> Loaded dasa as Pandas DataFrame
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages,categories,on='id')
+    return df 
 
 def clean_data(df):
-
-    categories = df.categories.str.split(pat = ';', expand = True)
-    row = categories.iloc[0,:]
-    category_colnames = row.apply(lambda str : str[:-2])
+    """
+    Clean Data function
+    
+    Arguments:
+        df -> raw data Pandas DataFrame
+    Outputs:
+        df -> clean data Pandas DataFrame
+    """
+    categories = df.categories.str.split(pat=';',expand=True)
+    firstrow = categories.iloc[0,:]
+    category_colnames = firstrow.apply(lambda x:x[:-2])
     categories.columns = category_colnames
     for column in categories:
-        categories[column] = categories[column].astype(str).apply(lambda str: str[-1])
-        categories[column] = categories[column].astype(int)
-    df.drop('categories', axis = 1, inplace = True)
-    df = pd.concat([df, categories], axis = 1)
-    df.drop_duplicates(inplace = True)
-
+        categories[column] = categories[column].str[-1]
+        categories[column] = categories[column].astype(np.int)
+    df = df.drop('categories',axis=1)
+    df = pd.concat([df,categories],axis=1)
+    #df = pd.concat([df,pd.get_dummies(df.genre)],axis=1)
+    #df = df.drop(['genre','social'],axis=1) 
+    df = df.drop_duplicates()
     return df
 
-
 def save_data(df, database_filename):
-    engine = create_engine('sqlite:///DisasterResponse.db')
-    df.to_sql('DisasterResponse', engine, index=False)
-    print("Data was saved to {} in the DisasterResponse table".format(database_filename))
+    """
+    Save Data function
+    
+    Arguments:
+        df -> Clean data Pandas DataFrame
+        database_filename -> database file (.db) destination path
+    """
+    engine = create_engine('sqlite:///'+ database_filename)
+    df.to_sql('df', engine, index=False)
+    pass  
+
 
 def main():
+    """
+    Main Data Processing function
+    
+    This function implement the ETL pipeline:
+        1) Data extraction from .csv
+        2) Data cleaning and pre-processing
+        3) Data loading to SQLite database
+    """
+    print(sys.argv)
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
@@ -42,12 +88,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-
+        
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-
+        
         print('Cleaned data saved to database!')
-
+    
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
